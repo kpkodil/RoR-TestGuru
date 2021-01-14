af8004a4d95035fae719931f277c21ccebcb3f36
kpkodil <kpkopb@gmail.com>
2021-01-14 13:59:37 +0300

    Создан сиды для исходных данных в таблице. Создан инстанс метод для User, который возвращает тесты, проходимые/законченные пользователем(принимает уровень теста в качестве аргумента). Создан метод класса Test, который выводит названия тестов в порядке убывания id  по необходимой категори (принимает название категории в качестве аргумента)

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,6 @@
 class Test < ApplicationRecord
+  def self.desc_sorted_with_category(category_title)
+    id_of_required_category = Category.where(title: category_title)
+    self.where(category_id: id_of_required_category).order(id: :desc).pluck(:title)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  def passing_tests(test_level)
+    id_of_tests_with_level = Test.where(level: test_level)
+    id_of_passing_tests = UserTest.where(user_id: self.id, test_id: id_of_tests_with_level)
+    pp Test.where(id: id_of_passing_tests)
+  end
 end
