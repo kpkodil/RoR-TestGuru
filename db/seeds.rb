d5208e27acc09cecff2175b6c661af37780a638f
kpkodil <kpkopb@gmail.com>
2021-01-16 22:12:54 +0300

    Убраны ненужные переменные в seeds, удален случайно созданный файл fa.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,47 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-if Test.all.empty? 
-  Category.create!([
+ 
+unless Test.exists?
+  categories = Category.create!([
     {title: "Category1"},
     {title: "Category2"}
   ])
 
-  User.create!([
+  users = User.create!([
     {name: "User1"},
     {name: "User2"}
   ])
 
-  Test.create!([
-    {title: "Test1", category_id: 1, author_id: 1},
-    {title: "Test2", category_id: 2, author_id: 2},
-    {title: "Test3", category_id: 1, author_id: 2}
+  tests = Test.create!([
+    {title: "Test1", category_id: categories[0].id, author_id: users[0].id},
+    {title: "Test2", category_id: categories[1].id, author_id: users[1].id},
+    {title: "Test3", category_id: categories[0].id, author_id: users[1].id}
   ])
 
-  Question.create!([
-    {title: "Question1", test_id: 1},
-    {title: "Question2", test_id: 1},
-    {title: "Question3", test_id: 2},
-    {title: "Question4", test_id: 2},
-    {title: "Question5", test_id: 3},
-    {title: "Question6", test_id: 3}
+  questions = Question.create!([
+    {title: "Question1", test_id: tests[0].id},
+    {title: "Question2", test_id: tests[0].id},
+    {title: "Question3", test_id: tests[1].id},
+    {title: "Question4", test_id: tests[1].id},
+    {title: "Question5", test_id: tests[2].id},
+    {title: "Question6", test_id: tests[2].id}
   ])
 
   Answer.create!([
-    {title: "Answer1", question_id: 1},
-    {title: "Answer2", question_id: 1},
-    {title: "Answer3", question_id: 2},
-    {title: "Answer4", question_id: 2},
-    {title: "Answer5", question_id: 3},
-    {title: "Answer6", question_id: 4},
-    {title: "Answer7", question_id: 5},
-    {title: "Answer8", question_id: 6}
+    {title: "Answer1", question_id: questions[0].id},
+    {title: "Answer2", question_id: questions[0].id},
+    {title: "Answer3", question_id: questions[1].id},
+    {title: "Answer4", question_id: questions[1].id},
+    {title: "Answer5", question_id: questions[2].id},
+    {title: "Answer6", question_id: questions[3].id},
+    {title: "Answer7", question_id: questions[4].id},
+    {title: "Answer8", question_id: questions[5].id}
   ])
 
   UserTest.create!([
-    {user_id: 1, test_id: 1},
-    {user_id: 1, test_id: 2},
-    {user_id: 1, test_id: 3}
+    {user_id: users[0].id, test_id: tests[0].id},
+    {user_id: users[0].id, test_id: tests[1].id},
+    {user_id: users[0].id, test_id: tests[2].id}
   ])
 end
