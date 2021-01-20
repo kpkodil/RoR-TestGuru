3bd88f3458c6513e7c3a137abba6819d8fc28006
kpkodil <kpkopb@gmail.com>
2021-01-20 14:10:36 +0300

    Убраны комментарии после Rubocop

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Question < ApplicationRecord
   has_many :answers
   belongs_to :test
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class User < ApplicationRecord
   has_many :tests_users
   has_many :tests, through: :tests_users
