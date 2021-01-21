743925e4f40b7ccce5529226612e340cd5a9be21
kpkodil <kpkopb@gmail.com>
2021-01-21 16:53:10 +0300

    belongs_to и has_many разделены. Настроены опции dependent: :destroy

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,14 +1,18 @@
 class Test < ApplicationRecord
+
   belongs_to :category
+
   belongs_to :author, class_name: 'User'
 
   has_many :questions, dependent: :destroy
+
   has_many :tests_users, dependent: :destroy
-  has_many :users, through: :users, dependent: :destroy
+
+  has_many :users, through: :users
 
   def self.desc_sorted_with_category(category_title)
     joins(:category)
-      .where({ category: { title: category_title } })
+      .where(categories: { title: category_title })
       .order(id: :desc)
       .pluck(:title)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,15 @@
 class User < ApplicationRecord
-  has_many :tests_users, dependent: :destroy
+
+  has_many :tests_users
+
   has_many :tests, through: :tests_users, dependent: :destroy
+  
   has_many :own_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
 
   def passing_tests_level(tests_level)
     Test
       .joins(:tests_users)
-      .where({ tests: { level: tests_level } })
-      .where({ tests_users: { user_id: id } })
+      .where(level: tests_level)
+      .where(tests_users: { user_id: id })
   end
 end
