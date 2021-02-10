8f9028abbda10eda0891c474a401bc2bad5cb091
kpkodil <kpkopb@gmail.com>
2021-02-10 13:46:08 +0300

    Добавлен вывод флеш сообщения с first_name и last_name пользователя.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,23 @@
 class ApplicationController < ActionController::Base
-
   protect_from_forgery with: :exception
-  
- def after_sign_in_path_for(guest)
-    if current_user.is_a?(Admin)
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    attributes = %i[first_name last_name]
+    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+  end
+
+  def after_sign_in_path_for(resource)
+    unless resource.first_name.empty? || resource.last_name.empty?
+      flash[:notice] = "Привет, #{resource.first_name} #{resource.last_name}!"
+    end
+    if resource.is_a?(Admin)
       admin_tests_path
     else
-      root_path
+      super
     end
-  end  
+  end
 end
